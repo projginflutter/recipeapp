2232c14e0406e2379d9d119dc3543df6ff90ed1b
pillaipakkam <saranath010@gmail.com>
2019-08-24 15:41:50 -0700

    Recipe card stateless

---
+++ styles.dart
@@ -0,0 +1,31 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+abstract class Styles {
+
+  static const cardTitleText = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 0.9),
+    fontFamily: 'NotoSans',
+    fontSize: 24,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const cardDescriptionText = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 0.9),
+    fontFamily: 'NotoSans',
+    fontSize: 16,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const ColorFilter desaturatedColorFilter =
+  // 222222 is a random color that has low color saturation.
+  ColorFilter.mode(Color(0xFF222222), BlendMode.saturation);
+
+}
---
+++ widgets/recipe_card.dart
@@ -0,0 +1,99 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:jakaa/styles.dart';
+
+/*
+Pass the stful card widget to this stateless widget to alter image props
+ */
+class FrostyBackground extends StatelessWidget {
+  const FrostyBackground({
+    this.color,
+    this.intensity = 2,
+    this.child,
+  });
+
+  final Color color;
+  final double intensity;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: intensity, sigmaY: intensity),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: color,
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
+
+class RecipeCard extends StatelessWidget {
+  //TODO change this to get model and relevant recipe data
+  RecipeCard(this.name, this.description, this.path);
+
+  /// Recipe Name
+  final String name;
+  //Short description
+  final String description;
+  //Image Asset Path
+  final String path;
+
+  Widget _buildDetails() {
+
+    return FrostyBackground(
+      color: Color(0x90ffffff),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              this.name,
+              style: Styles.cardTitleText,
+            ),
+            Text(
+              this.description,
+              style: Styles.cardDescriptionText,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Semantics(
+          label: 'A card background featuring ${this.name}',
+          child: Container(
+            height: 250,
+            width:250,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(15)),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                colorFilter:null,
+                image: AssetImage(
+                  this.path,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: _buildDetails(),
+        ),
+      ],
+    );
+  }
+}
